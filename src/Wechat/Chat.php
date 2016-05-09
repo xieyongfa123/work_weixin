a12b7c5a2b94b3c13c0359498cc9cd8a7606b998
ranjiaxiang <ranjiaxiang>
2016-05-09 10:30:57 +0800

    support chat api

---
+++ src/Wechat/Chat.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace Stoneworld\Wechat;
+
+/**
+ * 会话
+ */
+class Group
+{
+    const API_GET         = 'https://qyapi.weixin.qq.com/cgi-bin/chat/get';
+    const API_CREATE      = 'https://qyapi.weixin.qq.com/cgi-bin/chat/create';
+    const API_UPDATE      = 'https://qyapi.weixin.qq.com/cgi-bin/chat/update';
+    const API_QUIT        = 'https://qyapi.weixin.qq.com/cgi-bin/chat/quit';
+    const API_CLEARNOTIFY = 'https://qyapi.weixin.qq.com/cgi-bin/chat/clearnotify';
+    const API_SEND        = 'https://qyapi.weixin.qq.com/cgi-bin/chat/send';
+    const API_SETMUTE     = 'https://qyapi.weixin.qq.com/cgi-bin/chat/setmute';
+
+    /**
+     * Http对象
+     *
+     * @var Http
+     */
+    protected $http;
+
+    /**
+     * constructor
+     *
+     * @param string $appId
+     * @param string $appSecret
+     */
+    public function __construct($appId, $appSecret)
+    {
+        $this->http = new Http(new AccessToken($appId, $appSecret));
+    }
+
+    /**
+     * 创建会话
+     * @param  string $chatid    会话id
+     * @param  string $name      会话标题
+     * @param  string $owner     管理员userid
+     * @param  array  $userlist  会话成员列表
+     * @return integer           部门id
+     */
+    public function create($chatid, $name, $owner, $userlist)
+    {
+        $params = array(
+            'chatid' => $chatid,
+            'name' => $name,
+            'owner' => $owner,
+            'userlist' => $userlist
+        );
+
+        $response = $this->http->jsonPost(self::API_CREATE, $params);
+
+        return $response;
+    }
+
+    /**
+     * 获取会话
+     * @param  string $chatid 会话id
+     * @return array
+     */
+    public function get($chatid)
+    {
+        $response = $this->http->get(self::API_GET . '?chatid=' . $chatid);
+
+        return $response['chat_info'];
+    }
+
+    /**
+     * 修改会话信息
+     * @param  string $chatid  会话id
+     * @param  string $op_user 操作人userid
+     * @param  string $name    会话标题
+     * @param  string $owner   管理员userid
+     * @return array
+     */
+    public function update($chatid, $op_user, $name = null, $owner = null, $add_user_list = array(), $del_user_list = array())
+    {
+        $params = array(
+            'chatid' => $chatid,
+            'op_user' => $op_user,
+            'name' => $name,
+            'owner' => $owner,
+            'add_user_list' => $add_user_list,
+            'del_user_list' => $del_user_list
+        );
+
+        return $this->http->jsonPost(self::API_UPDATE, $params);
+    }
+
+    /**
+     * 退出会话
+     *
+     * @param int $id
+     *
+     * @return array
+     */
+    public function quit($chatid, $op_user)
+    {
+        $params = array(
+            'chatid' => $chatid,
+            'op_user' => $op_user
+        );
+
+        return $this->http->jsonPost(self::API_QUIT, $params);
+    }
+
+    /**
+     * 清除会话未读状态
+     *
+     * @param  int   $id   会话所有者的userid
+     * @param  array $chat 会话
+     * @return array
+     */
+    public function clearnotify($chatid, $chat)
+    {
+        $params = array(
+            'chatid' => $chatid,
+            'chat' => $chat
+        );
+
+        return $this->http->jsonPost(self::API_CLEARNOTIFY, $params);
+    }
+
+    /**
+     * 发消息
+     *
+     * @param  array  $type    接收人类型：single|group，分别表示：群聊|单聊
+     * @param  array  $id      接收人的值，为userid|chatid，分别表示：成员id|会话id
+     * @param  string $sender  发送人
+     * @param  string $msgtype 消息类型
+     * @param  array  $content 消息内容
+     * @return array
+     */
+    public function send($type, $id, $sender, $msgtype, $content)
+    {
+        $params = array(
+            'receiver' => array(
+                'type' => $type,
+                'id' => $id
+            ),
+            'sender' => $sender,
+            'msgtype' => $msgtype,
+            $msgtype => $content
+        );
+
+        return $this->http->jsonPost(self::API_SEND, $params);
+    }
+
+    /**
+     * 设置成员新消息免打扰
+     *
+     * @param  array $user_mute_list 成员新消息免打扰参数，数组，最大支持10000个成员
+     * @return array
+     */
+    public function setmute($user_mute_list)
+    {
+        $params = array(
+            'user_mute_list' => $user_mute_list,
+        );
+
+        return $this->http->jsonPost(self::API_SETMUTE, $params);
+    }
+}
