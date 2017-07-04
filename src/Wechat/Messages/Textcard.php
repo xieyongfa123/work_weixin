33d58d1f694098c31360176e5ecf6a4d657ff302
ws <wangshuai112465@pwrd.com>
2017-07-04 08:38:29 +0800

    add 文本卡片消息

---
+++ src/Wechat/Messages/Textcard.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Stoneworld\Wechat\Messages;
+
+/**
+ * 文本卡片消息
+ *
+ * @property string $media_id
+ */
+class Textcard extends BaseMessage
+{
+
+    /**
+     * @var array
+     */
+    protected $properties = array(
+        'title',
+        'description',
+        'url'
+    );
+
+    /**
+     * 生成主动消息数组
+     *
+     * @return array
+     */
+    public function toStaff()
+    {
+        return array(
+            'Textcard' => array(
+                'title'          => $this->title,
+                'description'    => $this->description,
+                'url'            => $this->url,
+            ),
+        );
+    }
+
+    /**
+     * 生成回复消息数组
+     *
+     * @return array
+     */
+    public function toReply()
+    {
+        $response = array(
+            'Textcard' => array(
+                'title'          => $this->title,
+                'description'    => $this->description,
+                'url'            => $this->url,
+            ),
+        );
+
+        return $response;
+    }
+}
